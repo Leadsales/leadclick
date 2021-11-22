6860fc0a962c16333cc29cf16f052fcdd6d3d7e2
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-11-21 22:07:24 -0600

    fix: :rotating_light: remove nested ternary expression

--- src/components/leadclick/leadclick.tsx
+++ src/components/leadclick/leadclick.tsx
@@ -26,74 +26,88 @@ export class Leadclick {
   /**
    * The background color
    */
-   @Prop() bgcolor: string;
-   /**
+  @Prop() bgcolor: string;
+  /**
    * The amount of connected integrations
    */
-   @Prop() integrations: number;
+  @Prop() integrations: number;
 
-   @State() showOptions = false;
+  @State() showOptions = false;
 
-   readonly svg = "whatsapp-icon.svg";
-   readonly buttons = ['instagram', 'facebook', 'whatsapp']
-
+  readonly whatsappSVG = getAssetPath(`./assets/whatsapp-icon.svg`);
+  readonly facebookSVG = getAssetPath(`./assets/messenger-icon.svg`);
+  readonly instagramSVG = getAssetPath(`./assets/instagram-icon.svg`);
+  readonly buttons = ['instagram', 'facebook', 'whatsapp'];
 
   // Helper functions
   openWhatsApp() {
     window.open(this.WAUrl, '_blank');
-    return
+    return;
   }
 
   openFacebook() {
     window.open(this.FBUrl, '_blank');
-    return
+    return;
   }
 
   openInstragram() {
     window.open(this.IGUrl, '_blank');
-    return
+    return;
   }
-
 
   clickHandler() {
-     if(this.integrations === 1){
-       if(this.WAUrl) this.openWhatsApp();
-       if(this.FBUrl) this.openFacebook();
-       if(this.IGUrl) this.openInstragram();
-     }else{
-       this.showOptions = !this.showOptions;
-     }
+    if (this.integrations === 1) {
+      if (this.WAUrl) this.openWhatsApp();
+      if (this.FBUrl) this.openFacebook();
+      if (this.IGUrl) this.openInstragram();
+    } else {
+      this.showOptions = !this.showOptions;
+    }
   }
-
-
 
   render() {
     let options = null;
     if (this.showOptions) {
-        options = (
-          <div class="card" >
-            <p>👋 ¿Por qué canal te gustaría comunicarte con nosotros?</p>
-            {this.buttons.map((btn, index, arr) => (
-                <div class="row_item">
-                    <img class="image" src={btn == 'instagram' ? getAssetPath(`./assets/instagram-icon.svg`) : btn == 'facebook' ? getAssetPath(`./assets/messenger-icon.svg`) : getAssetPath(`./assets/whatsapp-icon.svg`)} />
-                    <p class="integration">{btn}</p>
-                    {index !== arr.length-1 && <hr/>}
-                </div>
-            ))}
-                </div>
+      options = (
+        <div class="card">
+          <p>👋 ¿Por qué canal te gustaría comunicarte con nosotros?</p>
+          {this.buttons.map((btn, index, arr) => {
+            let icon;
+            if (btn === 'instagram') {
+              icon = this.instagramSVG;
+            }
+            if (btn === 'whatsapp') {
+              icon = this.whatsappSVG;
+            }
+            if (btn === 'facebook') {
+              icon = this.facebookSVG;
+            }
+            return (
+              <div class="row_item">
+                <img class="image" src={icon} />
+                <p class="integration">{btn}</p>
+                {index !== arr.length - 1 && <hr />}
+              </div>
             );
+          })}
+        </div>
+      );
     }
 
     return (
       <div class="wrapper">
         {options}
-        <div class="button" style={{backgroundColor: this.bgcolor ? this.bgcolor: "#3F40C2"}} onClick={this.clickHandler.bind(this)}>
+        <div class="button" style={{ backgroundColor: this.bgcolor ? this.bgcolor : '#3F40C2' }} onClick={this.clickHandler.bind(this)}>
           <img class="image" src={getAssetPath(`./assets/whatsapp-icon.svg`)} />
           <p class="cta-text">{this.cta}</p>
         </div>
-        <p class="by">⚡️by <a class="link" href="https://leadsales.io/">leadsales.io</a></p>
+        <p class="by">
+          ⚡️by{' '}
+          <a class="link" href="https://leadsales.io/">
+            leadsales.io
+          </a>
+        </p>
       </div>
     );
   }
 }
-
