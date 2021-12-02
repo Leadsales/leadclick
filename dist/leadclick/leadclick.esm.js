eb8ba8b2c7d13361b1dc49f1364351f3432523fa
Eduardo Rojas Alvarez <eduardorojasalvarez@comamosya.com>
2021-12-02 14:14:13 -0600

    fix: removed dist

--- dist/leadclick/leadclick.esm.js
+++
@@ -1,1 +0,0 @@
-import{p as o,b as r}from"./p-e23fcbc6.js";(()=>{const r=import.meta.url,t={};return""!==r&&(t.resourcesUrl=new URL(".",r).href),o(t)})().then((o=>r([["p-82ef2f4b",[[1,"leadclick-widget",{cta:[1],instructions:[1],waurl:[1],igurl:[1],fburl:[1],bgcolor:[1],fontcolor:[1],integrations:[2],showOptions:[32]}]]]],o)));
